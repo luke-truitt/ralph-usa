107a9c135d63a4b76af2eff5a7a40adcc5d4a3d0
LUKE TRUITT <lot@duke.edu>
2020-02-14 13:49:31 -0500

    updated files

--- algDev/lstm.py
+++ algDev/lstm.py
@@ -1,16 +1,19 @@
-import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import BatchNormalization, LeakyReLU, LSTM, Dense, Dropout
+# import pydot
+# from gensim.models import KeyedVectors
+from keras.layers import BatchNormalization, LeakyReLU, LSTM, Dense, Dropout, Flatten, Input, concatenate
 from keras.utils import plot_model
 from gen_lstm_data import gen_data, gen_labels, get_data_labelled
+import numpy as np
 
 # input should be [batch_size, time_steps, features] = [30, 19, 25]
 # labels = 10 classes
 
-(X_train, y_train, X_test, y_test) = gen_data(eq="VSLR", verbose=True)
-print(y_train[0])
-print(X_train[0])
+
+(X_train, y_train, X_test, y_test) = gen_data(eq = "VSLR", verbose= True)
+# print(y_train[0])
+# print(X_train[0])
 
 
 # first model (w/o text pipeline)
@@ -24,11 +27,12 @@ def create_model1a():
     model.add(Dense(28))
     model.add(LeakyReLU())
     model.add(BatchNormalization())
-    model.add(Dense(10, activation="softmax"))
+    model.add(Dense(10, activation = "softmax")) 
     return model
 
 
 model1a = create_model1a()
+# keras.utils.plot_model(model1a, 'my_first_model.png')
 
 
 def create_model1b():
@@ -50,56 +54,9 @@ model1b = create_model1b()
 # plot_model(model1b, to_file='model1b.png')
 
 model1a.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy', 'mae', 'mse'])
-model1a.fit(X_train, y_train, batch_size=30, epochs=500, verbose=2, validation_split=.2)
-score = model1a.evaluate(X_test, y_test, batch_size=30, verbose=2)
+model1a.fit(X_train, y_train, batch_size=32, epochs=500, verbose=2, validation_split=.2)
+score = model1a.evaluate(X_test, y_test, batch_size=32, verbose=2)
 print(score)
 
-# model1b.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy', 'mae', 'mse'])
-# model1b.fit(X_train, y_train, batch_size=30, epochs=500, verbose= 2, validation_split= .2)
-# score = model1b.evaluate(X_test, y_test, batch_size=30, verbose=2)
-# print(score)
-
-# we want labels to look like [batch, 19, 1]
 
 
-# from gensim.models.word2vec import Word2Vec
-# import gensim.downloader as api
-
-# import shorttext
-
-# create model to reduce dimensionality: LSTM -> Dense + Regularization
-# input should be [batch_size, time_steps, features]
-# labels = ??
-
-model_inputNum = layers.Input(shape=(19, 125))  # assuming we have 125 numerical features per time-step
-model_inputText = layers.Input(shape=(19, 200))  # assuming we have 200 words per time-step
-
-# for numerics
-LSTM = layers.LSTM(125, dropout=.2)(model_inputNum)
-Batch_Norm = layers.BatchNormalization()(LSTM)
-
-# for text
-
-# wvmodel = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz',
-#                                             binary=True)
-
-# api.load("GoogleNews-vectors-negative300.bin")
-# model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-# model = KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)(model_inputText)
-# Embed = api.load("word2vec-google-news-300")
-# Drop = layers.Dropout(.2)(wvmodel)
-
-# concatenate numerical, text inputs
-# Concat = layers.concat([Drop, Batch_Norm], axis=0)
-
-# Dense1 = layers.Dense(64)(Concat)
-Dense1 = layers.Dense(64)(Batch_Norm)
-LR = layers.LeakyRelu()(Dense1)
-Drop = layers.Dropout(.2)(Dense1)
-Dense2 = layers.Dense(28, activation='relu')(Drop)
-model_output = layers.Dense(1, activation="sigmoid")(Dense2)  # use softmax if not binary, sigmoid otherwise
-
-model = tf.keras.Model([model_inputNum, model_inputText], model_output)
-model.summary()
-
-keras.utils.plot_model(model, 'my_first_model.png')
