c8458e5f0ac24da0458f8e109df5f62ef5dbebfc
Cole Hattler <cwh31@duke.edu>
2020-02-17 14:16:30 -0500

    Started creating finance calculations class

---
+++ algDev/models/finance.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import math
+import numpy as np
+import statistics as stat 
+
+
+
+class Finance:
+
+    """
+    Contains methods to calculate important statistical characeristics
+    of securities and their relationships to each other to build efficient
+    portfolios
+    """
+
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def mean(eq, days = 500):
+        pass
+
+    @staticmethod
+    def variance(eq, days = 500):
+        pass
+        return 0
+
+    @staticmethod
+    def covariance(eq1, eq2, days = 500):
+        pass
+
+    @staticmethod
+    def stddev(eq, days = 500):
+        return math.sqrt(Finance.variance(eq, days))
+    
+    @staticmethod
+    def correlation(eq1, eq2, days = 500):
+        pass
+
+    
+
+
+     
