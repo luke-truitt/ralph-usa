1caeebbbe3f91bb85400a91d4cceba70742fff67
Cole Hattler <cwh31@duke.edu>
2020-04-22 12:29:00 -0400

    small erro fix

--- algDev/models/asset_strategy.py
+++ algDev/models/asset_strategy.py
@@ -5,8 +5,8 @@ class AssetStrategy:
     def __init__(self, asset_allocation, close_type='threshold'):
         self.asset_allocation = asset_allocation
 
-        self.close_types = 
-        assert close_type in self.algorithm_types = ['threshold', 'daily']
+        self.close_types = ['threshold', 'daily']
+        assert close_type in self.close_types
         self.close_type = close_type
 
     def allocate(self, date, positions, predictions, verbose=False):
